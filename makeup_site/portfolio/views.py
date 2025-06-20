54b585a3845493f0f8d8c0d311e3e9e40df6af94
ankit1350 <171828455+ankit1350@users.noreply.github.com>
2025-06-20 23:36:33 +0530

    first commit

---
+++ makeup_site/portfolio/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+
+def home(request):
+    return render(request, 'portfolio/home.html')
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def services(request):
+    return render(request, 'portfolio/services.html')
+
+def gallery(request):
+    return render(request, 'portfolio/gallery.html')
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
